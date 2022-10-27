182578093056c9cf011831326dbdb0847bd2e5d7
touazi aimen ryad <ia_touazi@esi.dz>
2022-10-27 16:56:35 +0200

    controle bar added needs some responsivness

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import './App.css'
+import Navbar from './components/Navbar'
 import Card from './components/Card'
+import Grid from './components/Grid'
 import bg from './assets/img/photo1.jpeg'
+
 function App() {
   return (
     <div className="App">
-     <Navbar></Navbar>
-     <div className='placeholder'><Card img={bg} alt="jfjf" name="Some name of grid goes here  " nb_comptence="123"></Card></div>
+      <Grid></Grid>
     </div>
   );
 }
---
+++ src/components/Grid.js
@@ -0,0 +1,45 @@
+import '../styles/grid.css'
+import more_icon from '../assets/icons/Moreo.svg'
+const Grid = () => {
+    return ( 
+        <div className='grid-container'>
+            <p className='grid-title'>Grille</p>
+            <div className='grid-control'>
+
+                <div className='grid-search'>
+                    
+                    <div className='select'>
+                        <select name="Recents" >
+                            <option>Recents</option>
+                            <option>1</option>
+                            <option>1</option>
+                        </select>
+                        <div className="select_arrow"></div>
+                    </div>
+
+                   <div className='select'>
+                        <select name="Tous" >
+                             <option>Tous</option>
+                             <option>1</option>
+                             <option>1</option>
+                         </select>
+                         <div className="select_arrow"></div>
+                   </div>
+                    
+                    <input type="text" placeholder="Rechercher par mot-clé ou tag"></input>
+                    
+                    <a href='#' className='more'><img src={more_icon} alt="icon more"></img></a>
+                </div>
+                
+                <button>Diffusion</button>
+                
+               
+            </div>
+            <div className='grid'>
+                {/* here put your cards */ }
+            </div>
+        </div>
+     );
+}
+ 
+export default Grid;
